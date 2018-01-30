7427112838e52f522f0512e69d4f8f07d32d976a
redbird <cs20131575@u.sogang.ac.kr>
2018-01-31 02:53:23 +0900

    modified code

--- analysis.py
+++ analysis.py
@@ -2,13 +2,14 @@ from textblob import TextBlob
 from googletrans import Translator
 
 translator = Translator()
-a = "나는 너가 정말 좋아."
+a = "오픈소스를 활용한 간단한 감성분석 엔진입니다."
 print(a)
-b = translator.translate(a)
-b = b.text
-#The polarity score is a float within the range [-1.0, 1.0].
-analysis = TextBlob(b)
-print("극성 감성 분석은 [-1.0, 1.0] 사이의 소수 입니다.")
-print("극성 감성 분석 결과 : ", analysis.sentiment.polarity)
-if analysis.sentiment.polarity < 0:
-    print("분석 결과 : 부정")
+engString = translator.translate(a)
+engString = engString.text
+print(engString)
+analysis = TextBlob(engString)
+print(analysis.sentiment.polarity)
+if analysis.sentiment.polarity >= 0:
+    print("분석결과 : 긍정 POSITIVE")
+elif analysis.sentiment.polarity < 0:
+    print("분석 결과 : 부정 NEGATIVE")
